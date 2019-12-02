51942a0c22d04b86afcfa75ae864244f72f11cc7
FelipeSchimidt <felipeschimidt85@gmail.com>
2019-12-02 16:42:31 -0300

    criação dos update e delete de Classes

--- backend/src/controllers/ClasseController.js
+++ backend/src/controllers/ClasseController.js
@@ -18,11 +18,36 @@ module.exports = {
     if (!classe) {
       return res.status(404).json({ error: "Classe não encontrada" });
     }
+
+    return res.status(200).json(classe);
   },
 
   async store(req, res) {
     const classe = await Classes.create(req.body);
 
     return res.json(classe);
+  },
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const classe = await Classes.findByPk(id);
+    if (!classe) {
+      return res.status(404).json({ error: "Classe não encontrada" });
+    }
+
+    await classe.update(req.body);
+
+    return res.status(200).json(classe);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const classe = await Classes.findByPk(id);
+    if (!classe) {
+      return res.status(404).json({ error: "Classe não encontrada" });
+    }
+    await classe.destroy();
   }
 };
