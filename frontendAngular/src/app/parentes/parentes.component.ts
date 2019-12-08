34642f34c86c6a35a08d2848414af40b64b7b944
Felipe Santos <felipeschimidt85@gmail.com>
2019-12-08 17:37:36 -0300

    criação da interface, modal form, metodos de acesso Parentes

--- frontendAngular/src/app/parentes/parentes.component.ts
+++ frontendAngular/src/app/parentes/parentes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import Parentes from './parentes';
+import { ServicesModule } from '../services/services.module';
 
 @Component({
   selector: 'app-parentes',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parentes.component.css']
 })
 export class ParentesComponent implements OnInit {
+  parentes: Parentes[];
+  parente: Parentes;
 
-  constructor() { }
+  parenteForm: FormGroup;
+
+  modoSalvar = 'post';
+  filtroLista: string;
+
+  constructor(private services: ServicesModule, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.getParentes();
   }
 
+  validation() {
+    this.parenteForm = this.fb.group({
+      nome: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(100),
+          Validators.minLength(5)
+        ]
+      ],
+      cpf: [''],
+      rg: [''],
+      nascimento: [''],
+      tipo: [''],
+      responsavel_financeiro: [''],
+      telefone: [''],
+      endereco: ['']
+    });
+  }
+
+  getParentes() {
+    this.services.getAllParentes().subscribe(
+      response => {
+        this.parentes = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  openModal(template: any) {
+    template.show();
+  }
 }
